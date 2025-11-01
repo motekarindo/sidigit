5573777ef4287094f4d7d77d7043ba37a7684320
wildanisme <wildanisme.com@gmail.com>
2025-11-01 12:46:59 +0700

    feat: Product implement soft delete and blameable on Product Material

--- app/Models/ProductMaterial.php
+++ app/Models/ProductMaterial.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Traits\LogsAllActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use RichanFongdasen\EloquentBlameable\BlameableTrait;
 
 class ProductMaterial extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, LogsAllActivity, BlameableTrait;
 
     protected $table = 'mst_product_materials';
 
